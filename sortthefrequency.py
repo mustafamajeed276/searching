ac36239d72d801e63f7c862cbf8c004ed8d2f79c
mustafamajeed276 <mmustafamajeed1@gmail.com>
2025-09-09 13:11:20 +0500

    sortbyfrequency

---
+++ sortthefrequency.py
@@ -0,0 +1,35 @@
+class Data:
+    def __init__(self,value,index,count=0):
+        self.value = value
+        self.index = index
+        self.count = count
+
+def sortByFrequencyAndIndex(arr):
+    if arr is None or len(arr) < 2:
+        return
+
+    hm={}
+    
+    
+    for i in range(len(arr)):
+        hm.setdefault(arr[i], Data(arr[i], i)).count += 1
+
+    values = [*hm.values()]    
+
+
+
+    values.sort(key=lambda x: (-x.count, x.index))
+
+    k = 0
+    for data in values:
+        for j in range(data.count):
+            arr[k] = data.value
+            k += 1 
+
+
+if __name__ == "__main__":
+
+    arr = [3,3,3,3,3,3,1,1,1,1,5,5,5,87,87,87,87,2,2,2,1,1,1,4,4]
+    print("Original Array:", arr)
+    sortByFrequencyAndIndex(arr)            
+print("Sorted Array:", arr)

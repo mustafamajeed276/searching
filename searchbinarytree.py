98cc1b51de132fd99b62b39388c568311b16a255
mustafamajeed276 <mmustafamajeed1@gmail.com>
2026-06-22 14:37:43 +0500

    real search

---
+++ searchbinarytree.py
@@ -0,0 +1,30 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+def search(root, key):
+
+    if root is None or root.val == key:
+        return root
+    
+    if root.val < key:
+        return search(root.right, key)
+    return search(root.left, key)
+
+root = Node(50)
+root.left = Node(30)
+root.right = Node(70)
+root.left.left = Node(20)
+root.left.right = Node(40)
+root.right.left = Node(60)
+root.right.right = Node(80)
+
+key = int(input("Enter the value to search: "))
+result = search(root, key)
+
+if result is not None:
+    print("key found", result.val)
+else:
+    print("key not found")
